ed44eedc1945b05f01da546f5761d929dacc18bc
Adam Mansfield <me@adammansfield.com>
2019-05-31 16:57:48 -0400

    Remove installation of dependencies in generate-client-and-sever.sh

--- scripts/generate-client-and-server.sh
+++ scripts/generate-client-and-server.sh
@@ -3,11 +3,10 @@
 main() {
   local -r scriptDir=$(dirname "$(readlink -f "$0")")
   local -r projectDir=$(readlink -f "$scriptDir/..")
-  local -r openApiSpec=$projectDir/api/openapi.yaml
-  local -r outputDirectory=$projectDir/gen
+  local -r openApiSpec="$projectDir/api/openapi.yaml"
+  local -r outputDirectory="$projectDir/gen"
 
-  install_missing_dependencies
-  get_openapi_generator $projectDir
+  get_openapi_generator "$projectDir/gen/openapi-generator"
   generate_client $openApiSpec $outputDirectory
   generate_server $openApiSpec $outputDirectory
 }
@@ -26,36 +25,14 @@ generate_server() {
 }
 
 get_openapi_generator() {
-  local -r projectDir=$1
+  local -r openApiGeneratorDir="$projectDir/gen/openapi-generator"
   if ! command -v openapi-generator-cli > /dev/null; then
-    openApiGeneratorDir=$projectDir/gen/openapi-generator
     if [[ ! -d $openApiGeneratorDir ]]; then
       mkdir -p $openApiGeneratorDir
     fi
     curl -s https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > $openApiGeneratorDir/openapi-generator-cli
     chmod u+x $openApiGeneratorDir/openapi-generator-cli
     export PATH=$PATH:$openApiGeneratorDir
-  fi
-}
-
-install_missing_dependencies() {
-  missingDependencies=
-  if ! command -v jq > /dev/null; then
-    missingDependencies="$missingDependencies jq"
-  fi
-
-  if ! command -v mvn > /dev/null; then
-    missingDependencies="$missingDependencies maven"
-  fi
-
-  if ! command -v python > /dev/null; then
-    missingDependencies="$missingDependencies python-minimal"
-  fi
-
-  if [ ! -z "$missingDependencies" ] ; then
-    echo "Running: sudo apt install -y $missingDependencies"
-    sudo apt update -qq
-    sudo apt install $missingDependencies
   fi
 }
 
