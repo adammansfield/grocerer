5613253729e98bc072797817f89b970bf8071f86
Adam Mansfield <me@adammansfield.com>
2019-06-06 14:38:52 -0400

    Remove build.sh

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-declare -r scriptDir=$(dirname "$(readlink -f "$0")")
-declare -r projectDir=$(readlink -f "$scriptDir/..")
-go get -d -v "$projectDir/internal/..."
-go build -a -installsuffix cgo -o "$projectDir/bin/openapi" "$projectDir/internal/."
