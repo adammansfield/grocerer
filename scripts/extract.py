a5a3a82020dfed2c64e350d6327acdc02e2f4e37
Adam Mansfield <me@adammansfield.com>
2019-06-13 14:36:51 -0400

    Add extract.py for extracting a file from a docker image

---
+++ scripts/extract.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Script for extracting a file or directory from a docker image"""
+
+import pathlib
+import subprocess
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        script_filename = pathlib.Path(__file__).resolve().name
+        print("USAGE: {} <docker_image> <source> <destination>".format(script_filename))
+        sys.exit()
+
+    docker_image = sys.argv[1]
+    source = sys.argv[2]
+    destination = sys.argv[3]
+
+    container = docker_create(docker_image)
+    docker_cp(container, source, destination)
+    docker_rm(container)
+
+def docker_create(docker_image):
+    result = subprocess.run(
+            ['docker', 'create', docker_image],
+            stdout=subprocess.PIPE)
+    result.check_returncode()
+    container = result.stdout.decode('utf-8')
+    return container.rstrip('\n')
+
+def docker_cp(container, source, destination):
+    container_source = "{}:{}".format(container, source)
+    result = subprocess.run(['docker', 'cp', container_source, destination])
+    result.check_returncode()
+
+def docker_rm(container):
+    result = subprocess.run(
+            ['docker', 'rm', container],
+            stdout=subprocess.DEVNULL)
+    result.check_returncode()
+
+main()
