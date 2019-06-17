a104db3e6c9293aaf3af3d366cd56c43c43e62ee
Adam Mansfield <me@adammansfield.com>
2019-06-17 16:48:32 -0400

    Use pathlib's touch in touch.py

--- scripts/touch.py
+++ scripts/touch.py
@@ -2,12 +2,7 @@
 
 """Cross-platform script for updating a file's timestamp"""
 
-import os
-import subprocess
+import pathlib
 import sys
 
-if os.name == 'nt':
-    command = "(ls {0}).LastWriteTime = Get-Date".format(sys.argv[1])
-    subprocess.run(['powershell', '-NoProfile', '–ExecutionPolicy', 'Bypass', command])
-else:
-    subprocess.run(['touch', sys.argv[1]])
+pathlib.Path(sys.argv[1]).touch()
