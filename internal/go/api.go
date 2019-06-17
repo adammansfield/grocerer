2de43c141c1c71fa3e87bf2b15b40f86b386bd89
Adam Mansfield <me@adammansfield.com>
2019-06-17 16:48:09 -0400

    Add logIn function

--- internal/go/api.go
+++ internal/go/api.go
@@ -2,7 +2,10 @@ package openapi
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 const (
@@ -10,7 +13,49 @@ const (
 	APIVersion = "1"
 	// BuildDate is the date of the build
 	BuildDate = "2019-06-14T12:23"
+
+	mainRawURL   = "https://www.ourgroceries.com"
+	signInRawURL = "https://www.ourgroceries.com/sign-in"
+	userAgent    = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36"
 )
+
+func logIn() error {
+	form := url.Values{}
+	form.Set("action", "sign-me-in")
+	form.Set("emailAddress", container.Config.Email)
+	form.Set("password", container.Config.Password)
+	form.Set("staySignedIn", "on")
+
+	request, err := http.NewRequest("POST", signInRawURL, strings.NewReader(form.Encode()))
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("Origin", mainRawURL)
+	request.Header.Add("Referer", signInRawURL)
+	request.Header.Add("User-Agent", userAgent)
+
+	response, err := container.HTTPClient.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code %d for %s", response.StatusCode, response.Request.URL.String())
+	}
+
+	signInURL, err := url.Parse(signInRawURL)
+	if err != nil {
+		return err
+	}
+	cookies := container.CookieJar.Cookies(signInURL)
+	if cookies == nil {
+		return fmt.Errorf("invalid credentials")
+	}
+
+	return nil
+}
 
 // GetVersion responds with the API version and build date
 func GetVersion(w http.ResponseWriter, r *http.Request) {
--- internal/go/container.go
+++ internal/go/container.go
@@ -1,14 +1,23 @@
 package openapi
 
+import (
+	"net/http"
+	"net/http/cookiejar"
+)
+
 // Container provides dependencies
 type Container struct {
-	Config Config
+	CookieJar  *cookiejar.Jar
+	Config     Config
+	HTTPClient http.Client
 }
 
 // NewContainer creates a Container with real interfaces
 func NewContainer() Container {
 	config, _ := LoadConfig()
-	return Container{config}
+	cookieJar, _ := cookiejar.New(nil)
+	httpClient := http.Client{Jar: cookieJar}
+	return Container{cookieJar, config, httpClient}
 }
 
 // TODO: remove `var container` when openapi-generator is no longer used
