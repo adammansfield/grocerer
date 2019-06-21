25c656febd38f1f114ce0fa34419e0e8ac5cbfaa
Adam Mansfield <me@adammansfield.com>
2019-06-21 15:06:45 -0400

    Add check for go and golint in install-pre-commit.py

--- scripts/git/install-pre-commit.py
+++ scripts/git/install-pre-commit.py
@@ -7,7 +7,21 @@ import sys
 
 def main():
     check_python3()
+    check_go()
+    check_golint()
     get_pre_commit_symlink().symlink_to(get_pre_commit_file())
+
+def check_go():
+    if shutil.which("go") == None:
+        print("ERROR: go is not installed")
+        print("To install: https://golang.org/doc/install")
+        sys.exit(1)
+
+def check_golint():
+    if shutil.which("golint") == None:
+        print("ERROR: golint is not installed")
+        print("To install: https://github.com/golang/lint")
+        sys.exit(1)
 
 def check_python3():
     if shutil.which("python3") == None:
@@ -15,19 +29,19 @@ def check_python3():
         print("Create a symlink 'python3' in PATH")
         sys.exit(1)
 
-def get_pre_commit_symlink():
+def get_pre_commit_symlink() -> pathlib.Path:
     result = subprocess.run(
             ['git', 'rev-parse', '--git-dir'],
             check=True,
             stdout=subprocess.PIPE)
     git_directory = result.stdout.decode('utf-8').rstrip('\n')
     pre_commit_symlink = pathlib.Path(git_directory) / 'hooks' / 'pre-commit'
-    if pre_commit_symlink.exists():
+    if pre_commit_symlink.exists() or pre_commit_symlink.is_symlink():
         print("ERROR: {} exists".format(pre_commit_symlink))
         sys.exit(1)
     return pre_commit_symlink
 
-def get_pre_commit_file():
+def get_pre_commit_file() -> pathlib.Path:
     git_scripts_directory = pathlib.Path(__file__).resolve().parent
     return git_scripts_directory / 'pre-commit.py'
 
