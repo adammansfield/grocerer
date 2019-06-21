2daaba0ce6fc5cbd26e9b2cf6707a3c2c79df177
Adam Mansfield <me@adammansfield.com>
2019-06-21 16:41:47 -0400

    Fix typo in comment

--- internal/go/api.go
+++ internal/go/api.go
@@ -130,7 +130,7 @@ func getLists(teamID string) ([]List, error) {
 	return yourListsResponseJSON.ShoppingLists, nil
 }
 
-// GetLists responsd with the grocery lists
+// GetLists responds with the grocery lists
 func GetLists(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
