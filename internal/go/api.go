560b1d226e5af8e7fee9870590ee6d594af65a97
Adam Mansfield <me@adammansfield.com>
2019-06-22 18:21:37 -0400

    Add POST /lists/{listID}/items to openapi.yaml

--- internal/go/api.go
+++ internal/go/api.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+// AddItem adds an item to a list
+func AddItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
+
 // GetLists responds with the grocery lists
 func GetLists(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
