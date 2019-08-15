16b278e606611fe05561f55fa2907501d135d2a8
Adam Mansfield <me@adammansfield.com>
2019-08-15 13:53:16 -0400

    Check if item was added in the end-to-end test
    - Refactor end to end test into multiple tests

--- pkg/ourgrocer/e2e_test.go
+++ pkg/ourgrocer/e2e_test.go
@@ -4,36 +4,82 @@ package ourgrocer_test
 
 import (
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/adammansfield/grocerer/pkg/ourgrocer"
 )
 
-const (
-	emailEnvVar = "OURGROCERIES_EMAIL"
-	passEnvVar  = "OURGROCERIES_PASSWORD"
-)
+// Test fixture for storing state between tests to avoid multiple logins
+type fixture struct {
+	Client ourgrocer.Client
+	Email  string           // Ourgroceries email
+	Item   string           // Item to be added using AddItem
+	Lists  []ourgrocer.List // Lists returned by GetLists
+	Pass   string           // Ourgroceries password
+}
+
+var f fixture
+
+func TestSetup(t *testing.T) {
+	f = newFixture(t)
+}
+
+func TestLogin(t *testing.T) {
+	err := f.Client.Login(f.Email, f.Pass)
+	ok(t, err)
+	assert(t, f.Client.TeamID != "", "teamID not found")
+}
+
+func TestGetLists(t *testing.T) {
+	var err error
+	f.Lists, err = f.Client.GetLists()
+	ok(t, err)
+	assert(t, len(f.Lists) > 0, "lists not found")
+}
 
 func TestAddItem(t *testing.T) {
-	email := os.Getenv(emailEnvVar)
-	pass := os.Getenv(passEnvVar)
-	assert(t, email != "", "Environment variable %s is empty or not set", emailEnvVar)
-	assert(t, pass != "", "Environment variable %s is empty or not set", passEnvVar)
-
-	client := ourgrocer.Client{}
-	err := client.Login(email, pass)
+	err := f.Client.AddItem(f.listID(), f.Item)
 	ok(t, err)
-	assert(t, client.TeamID != "", "teamID not found")
+}
 
-	lists, err := client.GetLists()
+func TestGetList(t *testing.T) {
+	items, err := f.Client.GetList(f.listID())
 	ok(t, err)
-	assert(t, len(lists) > 0, "lists not found")
+	assert(t, containsName(items, f.Item), "%s was not added to the grocery list", f.Item)
+}
 
-	listID := lists[0].ID
-	err = client.AddItem(listID, "sardines")
-	ok(t, err)
+func containsName(items []ourgrocer.Item, name string) bool {
+	for _, item := range items {
+		if strings.Contains(item.Value, name) {
+			return true
+		}
+	}
+	return false
+}
 
-	// TODO: check if sardines is on the list
-	_, err = client.GetList(listID)
-	ok(t, err)
+func (f *fixture) listID() string {
+	return f.Lists[0].ID
+}
+
+func getEmail(t *testing.T) string {
+	result := os.Getenv("OURGROCERIES_EMAIL")
+	assert(t, result != "", "Environment variable %s is empty or not set", "OURGROCERIES_EMAIL")
+	return result
+}
+
+func getPass(t *testing.T) string {
+	result := os.Getenv("OURGROCERIES_PASSWORD")
+	assert(t, result != "", "Environment variable %s is empty or not set", "OURGROCERIES_PASSWORD")
+	return result
+}
+
+func newFixture(t *testing.T) fixture {
+	f := fixture{}
+	f.Client = ourgrocer.Client{}
+	f.Email = getEmail(t)
+	// TODO: randomly generate item name
+	f.Item = "sardines"
+	f.Pass = getPass(t)
+	return f
 }
